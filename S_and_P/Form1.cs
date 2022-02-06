1cc5e6521fbc21b957322aecf699401c7130622f
Alex <Alex@DESKTOP-PIJTJQ7>
2022-02-06 13:17:07 +0300

    Добавьте файлы проекта.

---
+++ S_and_P/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace S_and_P
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double a, b, s, p;
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            s = a * b;
+            label5.Text = Convert.ToString(s);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            p = 2 * (a + b);
+            label6.Text = Convert.ToString(p);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            label5.Text = null;
+            label6.Text = null;
+        }
+    }
+}
